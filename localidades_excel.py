bf3cc063ca0533401b1705fc68a16879c8e512f7
lea.arturi <lea.arturi@gmail.com>
2021-08-20 09:30:48 -0300

    Fix problema error 403

--- localidades_excel.py
+++ localidades_excel.py
@@ -1,19 +1,19 @@
-import requests
-import pandas as pd
-import json
-import pathlib
-import sys
-sys.path.insert(0, '/modulos/')
 from modulos import ficheros
+import pandas as pd
+import sys
+
+from urllib.request import Request, urlopen
+
+sys.path.insert(0, '/modulos/')
 
 # Creo archivo de salida
 fichero = ficheros.Fichero('localidades.csv')
 
 # Lista de Provincias
-url_provincias = "http://www.inmuebles.clarin.com/Regiones/FindProvincias?contentType=json&idPais=1"
-headers = {"User-Agent":"Mozilla/5.0"}
-provincias_res = requests.get(url_provincias, headers=headers).text
-df = pd.read_json(url_provincias)
+url_provincias = 'http://www.inmuebles.clarin.com/Regiones/FindProvincias?contentType=json&idPais=3'
+req_provincias = Request(url_provincias, headers={'User-Agent': 'Mozilla/5.0'})
+provincias_res = urlopen(req_provincias).read()
+df = pd.read_json(provincias_res)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 id_list_prov = df['Id'].tolist()
@@ -21,10 +21,11 @@ provincia_list = df['Nombre'].tolist()
 
 # Lista de Partidos de la Provincia prov
 for prov in range(len(id_list_prov)):
-    url_partidos = "http://www.inmuebles.clarin.com/Regiones/FindPartidos?contentType=json&IdProvincia=" + str(id_list_prov[prov])
-    headers = {"User-Agent":"Mozilla/5.0"}
-    partidos_res = requests.get(url_partidos, headers=headers).text
-    df_partidos = pd.read_json(url_partidos)
+    url_partidos = "http://www.inmuebles.clarin.com/Regiones/FindPartidos?contentType=json&IdProvincia=" + \
+        str(id_list_prov[prov])
+    req_partidos = Request(url_partidos, headers={'User-Agent': 'Mozilla/5.0'})
+    partidos_res = urlopen(req_partidos).read()
+    df_partidos = pd.read_json(partidos_res)
     pd.set_option('display.max_columns', None)
     pd.set_option('display.max_rows', None)
     id_list_partidos = df_partidos['Id'].tolist()
@@ -32,11 +33,12 @@ for prov in range(len(id_list_prov)):
 
     # Hago una peticion por cada partido para extraer las localidades
     for part in range(len(id_list_partidos)):
-        url_localidades = "http://www.inmuebles.clarin.com/Regiones/FindLocalidades?contentType=json&IdPartido=" + str(id_list_partidos[part])
-
-        headers = {"User-Agent":"Mozilla/5.0"}
-        respuesta_res = requests.get(url_localidades, headers=headers).text
-        df_localidades = pd.read_json(url_localidades)
+        url_localidades = "http://www.inmuebles.clarin.com/Regiones/FindLocalidades?contentType=json&IdPartido=" + \
+            str(id_list_partidos[part])
+        req_localidades = Request(url_localidades, headers={
+                                  'User-Agent': 'Mozilla/5.0'})
+        localidades_res = urlopen(req_localidades).read()
+        df_localidades = pd.read_json(localidades_res)
         pd.set_option('display.max_columns', None)
         pd.set_option('display.max_rows', None)
         id_list_localidades = df_localidades['Id'].tolist()
@@ -45,20 +47,17 @@ for prov in range(len(id_list_prov)):
         # Por cada localidad hago un insert en el csv
         for loc in range(len(id_list_localidades)):
             fichero.append(
-                str(id_list_prov[prov]) + ';' + 
-                provincia_list[prov]  + ';' +  
-                str(id_list_partidos[part]) + ';' + 
+                str(id_list_prov[prov]) + ';' +
+                provincia_list[prov] + ';' +
+                str(id_list_partidos[part]) + ';' +
                 partidos_list[part] + ';' +
-                str(id_list_localidades[loc]) + ';' + 
-                localidades_list[loc] + ';' +  
+                str(id_list_localidades[loc]) + ';' +
+                localidades_list[loc] + ';' +
                 '\n')
 
-            print(str(id_list_prov[prov]) + ';' + 
-                provincia_list[prov]  + ';' +  
-                str(id_list_partidos[part]) + ';' + 
-                partidos_list[part] + ';' +
-                str(id_list_localidades[loc]) + ';' + 
-                localidades_list[loc] + ';')    
-
-
-       
+        print(str(id_list_prov[prov]) + ';' +
+              provincia_list[prov] + ';' +
+              str(id_list_partidos[part]) + ';' +
+              partidos_list[part] + ';' +
+              str(id_list_localidades[loc]) + ';' +
+              localidades_list[loc] + ';')
